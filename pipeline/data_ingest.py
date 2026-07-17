83080f886f4ac6369b5990fbdc1b9a4966f26017
Khanh2802 <149643898+Khanh2802@users.noreply.github.com>
2026-07-17 11:04:43 +0000

    pipeline code working

---
+++ pipeline/data_ingest.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import pandas as pd
+from sqlalchemy import create_engine
+from tqdm.auto import tqdm
+
+#!uv add sqlalchemy
+#!uv add psycopg2-binary
+#! uv add tqdm
+
+dtype = {
+    "VendorID": "Int64",
+    "passenger_count": "Int64",
+    "trip_distance": "float64",
+    "RatecodeID": "Int64",
+    "store_and_fwd_flag": "string",
+    "PULocationID": "Int64",
+    "DOLocationID": "Int64",
+    "payment_type": "Int64",
+    "fare_amount": "float64",
+    "extra": "float64",
+    "mta_tax": "float64",
+    "tip_amount": "float64",
+    "tolls_amount": "float64",
+    "improvement_surcharge": "float64",
+    "total_amount": "float64",
+    "congestion_surcharge": "float64"
+}
+
+parse_dates = [
+    "tpep_pickup_datetime",
+    "tpep_dropoff_datetime"
+]
+
+def run():
+    pg_user = 'root'
+    pg_password = 'root'
+    pg_host = 'localhost'
+    pg_port = '5432'
+    pg_database = 'Adventureworks2025'
+
+    year = 2021
+    month = 1
+
+    target_table=f'yellow_taxi_data_{year}/{month:02d}'
+
+    chunk_size = 100000
+
+    prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
+    url = f'{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz'
+    engine = create_engine(f'postgresql+psycopg://{pg_user}:{pg_password}@{pg_host}:{pg_port}/{pg_database}')
+
+    df_iter = pd.read_csv(
+        url,
+        dtype=dtype,
+        parse_dates=parse_dates,
+        iterator=True,
+        chunksize=chunk_size
+    )
+    
+    first=True
+    for df_chunk in tqdm(df_iter):
+        if first:
+            df_chunk.head(n=0).to_sql(
+                name=target_table, 
+                con=engine, 
+                if_exists='replace'
+            )
+            first=False
+        df_chunk.to_sql(
+            name=target_table, 
+            con=engine, 
+            if_exists='append'
+        )
+        print(len(df_chunk))
+
+if __name__ == '__main__':
+    run()
+
+
+
+
+
